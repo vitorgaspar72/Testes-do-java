24aa85eeb723e300c24a1d6f03414c8926969c5c
Vitor <Vitor@Vitor>
2021-08-17 22:48:03 -0300

    Selection Sort implementado

--- JavaRevisao/src/main/java/Sorts/SelectionSort.java
+++ JavaRevisao/src/main/java/Sorts/SelectionSort.java
@@ -1,21 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Sorts;
 
-/**
- *
- * @author Vitor
- */
+
+
 public class SelectionSort {
 
     /**
-     * @param args the command line arguments
+     * @trocam os elementos de acordo com a seleção
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[]teste={10,29,93,1,39,2,231,9,1239,23};       //vai ordenar do menor para o maior
+       
+        for(int i=0;i<teste.length;i++){
+            for(int j=i+1;j<teste.length;j++){
+                int menor=teste[i]; //variavel externa que recebe o valor se a casa anterior for maior que a proxima
+                if(menor>teste[j]){
+                    teste[i]=teste[j];
+                    teste[j]=menor;
+                    
+                }
+            }
+        }
+        
+        for(int i=0;i<teste.length;i++){
+            System.out.println("");
+            System.out.println(teste[i]);
+        }
+        
     }
     
+  
 }
